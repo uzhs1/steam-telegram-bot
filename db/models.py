3c13472f4ff9270d81220fff1815f1d02aaf4c5b
trip73 <mihail.karpinskii@mail.ru>
2022-10-22 13:39:50 +0300

    Add alembic

--- db/models.py
+++ db/models.py
@@ -3,6 +3,7 @@ import enum
 
 from dateutil.relativedelta import relativedelta
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import ENUM
 
 from .base import db
 
@@ -10,9 +11,16 @@ today = datetime.datetime.now()
 offer_ended = today + relativedelta(minutes=15)
 
 
-class OfferType(enum.Enum):
+@enum.unique
+class OfferTypeEnum(enum.Enum):
     buy = 'Buy'
     sell = 'Sell'
+
+
+@enum.unique
+class OfferStatusEnum(enum.Enum):
+    created_status = 'Created'
+    ended_status = 'Ended'
 
 
 class User(db.Model):
@@ -21,9 +29,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tg_id = db.Column(db.BigInteger, unique=True)
     name = db.Column(db.String(32))
-    offers = relationship("Offer")
+    offers = relationship('Offer')
+    transactions = relationship('Transaction')
 
     # Some methods here
+
+
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    trx_hash = db.Column(db.String(150), unique=True)
+    amount = db.Column(db.Float)
+    offers = relationship('Offer')
 
 
 class Offer(db.Model):
@@ -35,8 +54,9 @@ class Offer(db.Model):
     count_keys = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=today)
     ended_ad = db.Column(db.DateTime, default=offer_ended)
-    offer_type = db.Column(db.Enum(OfferType))
-    trx_hash = db.Column(db.String(255), unique=True)
+    offer_type = db.Column(ENUM(OfferTypeEnum))
+    offer_status = db.Column(ENUM(OfferStatusEnum))
+    trx_id = db.Column(db.Integer, db.ForeignKey('transactions.id'), nullable=True)
 
 
 class Keys(db.Model):
@@ -47,3 +67,5 @@ class Keys(db.Model):
     user_sell_price = db.Column(db.Float, default=1.66)
     user_max_buy = db.Column(db.Integer, default=1500)
     user_max_sell = db.Column(db.Integer, default=10)
+
+
